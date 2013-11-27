9c889b0826db4d7fc748f53f1e072182514a4ec8
reb3lzrr <boarderkoen@hotmail.com>
2013-11-27 10:13:26 +0100

    FormFail

---
+++ GDD_Game_Windows/GDD_Game_Windows/FormFail.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GDD_Game_Windows
+{
+    partial class FormFail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormFail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 212);
+            this.Name = "FormFail";
+            this.Text = "FormFail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ GDD_Game_Windows/GDD_Game_Windows/FormFail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDD_Game_Windows
+{
+    public partial class FormFail : FormScore
+    {
+        public FormFail()
+        {
+            InitializeComponent();
+
+            this.Button_Back.Text = "Retry";
+            this.label_Status.Text = "You died";
+            this.label_Score.Visible = false;
+            this.label_Highscore.Visible = false;
+
+            this.Button_Back.Click += Button_Back_Click;
+        }
+
+
+        void Button_Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
